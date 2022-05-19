c1db923907d68770b32ace8bc8afeef81709aae0
Sreenath285 <sreenath.285@gmail.com>
2022-05-19 17:47:14 +0530

    Using service layer getting greeting message

--- src/main/java/com/example/greetingapp/controller/GreetingRestController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingRestController.java
@@ -1,6 +1,10 @@
 package com.example.greetingapp.controller;
 
 import com.example.greetingapp.model.Greeting;
+import com.example.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +17,16 @@ public class GreetingRestController {
     private static final String template = "Hello %s";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/getGreeting")
+    public ResponseEntity<String> getGreetingMessage() {
+        return new ResponseEntity<String>(greetingService.getGreeting(), HttpStatus.OK);
     }
 }
